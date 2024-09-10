42402aef045b870e009af51c527f88687d5c1068
ktcv31 <tatiana.carvajal0831@gmail.com>
2024-09-10 18:26:29 -0500

    fixed search and saved books

---
+++ Develop/client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import Main from './components';  
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Main />
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ Develop/server/utils/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+  authMiddleware: function ({ req }) {
+    let token = req.headers.authorization || '';
+
+    if (token) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return { user: null };
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      return { user: data };
+    } catch {
+      console.log('Invalid token');
+      return { user: null };
+    }
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
