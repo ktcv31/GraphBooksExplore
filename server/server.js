af9b68b468a435ac62ca24004e23a82664ef329f
ktcv31 <tatiana.carvajal0831@gmail.com>
2024-09-10 21:46:52 -0500

    edit server

--- server/server.js
+++ server/server.js
@@ -22,11 +22,11 @@ server.start().then(() => {
   app.use(express.json());
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
   }
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 
   db.once('open', () => {
